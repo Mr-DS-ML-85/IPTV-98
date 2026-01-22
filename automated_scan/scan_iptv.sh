2814f85acbb9176b3783cdffec97ca77ee859335
Mr-DS-ML-85 <gojmanday@gmail.com>
2026-01-22 09:34:41 +0600

    added another link

--- automated_scan/scan_iptv.sh
+++ automated_scan/scan_iptv.sh
@@ -10,7 +10,7 @@ python3 rm-dupe.py collect/
 python3 m3u-vaildator.py collect/playlist_dedup.m3u
 # validate everything in collect, write to final, use ffprobe
 bash health.sh
-python3 merge_playlists.py collect/ --output final/playtv.m3u
+python3 merge_playlists.py collect/ --output final/play.m3u
 echo "Scraping and building m3u file done successfully filename: playtv.m3u in final"
 
 # 2) Post‑processing:
@@ -18,9 +18,9 @@ echo "Scraping and building m3u file done successfully filename: playtv.m3u in f
 rm -f collect/*
 
 # 2b) Move final/playtv.m3u to ../playlist, replacing old one
-if [ -f final/playtv.m3u ]; then
+if [ -f final/play.m3u ]; then
     rm -f ../playlist/playtv.m3u
-    mv final/playtv.m3u ../playlist/playtv.m3u
+    mv final/play.m3u ../playlist/playtv.m3u
     echo "Moved new playtv.m3u to playlist/"
 else
     echo "ERROR: final/playtv.m3u not found, nothing to move" >&2
