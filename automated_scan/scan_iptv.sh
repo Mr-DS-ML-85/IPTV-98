0621cd78751fd1cf449a8ff4677ed7a4b5ae2c17
Mr-DS-ML-85 <gojmanday@gmail.com>
2026-02-24 07:20:12 +0600

    up

--- automated_scan/scan_iptv.sh
+++ automated_scan/scan_iptv.sh
@@ -7,12 +7,12 @@ cd "$(dirname "$0")"
 
 # 1) Run the pipeline
 python3 ultra-iptv.py
-python3 rm-dupe.py collect/playlist.m3u
+#python3 rm-dupe.py collect/playlist.m3u
 # validate everything in collect, write to final, use ffprobe
 echo "Scraping and building m3u file done successfully filename: playtv.m3u in final"
 # 2) Post‑processing:
 # 2a) Clean collect folder
-cp collect/playlist.m3u  collect/play.m3u
+#cp collect/playlist.m3u  collect/play.m3u
 mv collect/play.m3u final/
 # 2b) Move final/playtv.m3u to ../playlist, replacing old one
 if [ -f final/play.m3u ]; then
